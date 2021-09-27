7164814235849825ca81edf1d2153f90e7842079
mickemohlin <mickemohlin@live.se>
2021-09-27 17:38:22 +0200

    First Commit

---
+++ Model/AI.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.IO;
+using BlazorConnect4.Model;
+using System.Threading.Tasks;
+
+namespace BlazorConnect4.AIModels
+{
+    [Serializable]
+    public abstract class AI
+    {
+        // Funktion för att bestämma vilken handling som ska genomföras.
+        public abstract int SelectMove(Cell[,] grid);
+
+        // Funktion för att skriva till fil.
+        public virtual void ToFile(string fileName)
+        {
+            using (Stream stream = File.Open(fileName, FileMode.Create))
+            {
+                var bformatter = new System.Runtime.Serialization.Formatters.Binary.BinaryFormatter();
+                bformatter.Serialize(stream, this);
+            }
+        }
+
+        // Funktion för att att läsa från fil.
+        protected static AI FromFile(string fileName)
+        {
+            AI returnAI;
+            using (Stream stream = File.Open(fileName, FileMode.Open))
+            {
+                var bformatter = new System.Runtime.Serialization.Formatters.Binary.BinaryFormatter();
+                returnAI = (AI)bformatter.Deserialize(stream);
+            }
+            return returnAI;
+
+        }
+
+    }
+
+
+
+    [Serializable]
+    public class RandomAI : AI
+    {
+        [NonSerialized] Random generator;
+
+        public RandomAI()
+        {
+            generator = new Random();
+        }
+
+        public override int SelectMove(Cell[,] grid)
+        {
+            return generator.Next(7);
+        }
+
+        public static RandomAI ConstructFromFile(string fileName)
+        {
+            RandomAI temp = (RandomAI)(AI.FromFile(fileName));
+            // Eftersom generatorn inte var serialiserad.
+            temp.generator = new Random();
+            return temp;
+        }
+    }
+}
